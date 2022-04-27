c96bccfffbba0665559eb60d2dfbb99a0f39557e
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-04-27 15:08:06 +0200

    fix: remove style tags

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="mb-5">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -17,20 +17,3 @@ defineProps<{
   test: 'foo' | 'bar';
 }>();
 </script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
