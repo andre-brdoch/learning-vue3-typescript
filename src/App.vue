8b0372e8ff4ed7a69a495ba57e89ce04d08c7535
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-04-28 10:03:58 +0200

    fix: delete wind speed

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
       <p>Time: {{ formatDate(new Date(item.time), 'HH:00') }}</p>
       <p>Temperature: {{ item.temperature }}{{ state.units?.temperature }}</p>
       <p>Clouds: {{ item.cloudcover }}{{ state.units?.cloudcover }}</p>
-      <p>Wind: {{ item.windspeed }}{{ state.units?.windspeed }}</p>
     </article>
   </div>
 </template>
@@ -26,13 +25,11 @@ interface ApiPayload {
     cloudcover: number[];
     temperature_2m: number[];
     time: string[];
-    windspeed_10m: number[];
   };
   hourly_units: {
     cloudcover: string;
     temperature_2m: string;
     time: string;
-    windspeed_10m: string;
   };
 }
 
@@ -40,7 +37,6 @@ interface HourData {
   temperature: number;
   cloudcover: number;
   time: string;
-  windspeed: number;
 }
 
 type Units = {
@@ -65,29 +61,23 @@ onMounted(async () => {
 
   const response = await axios(url);
   const data: ApiPayload = response.data;
-  console.log(data);
-  console.log(data.hourly);
   state.hoursData = Array.from(Array(24).keys()).map(i => {
     const {
       temperature_2m: temperatures,
       cloudcover: cloudcovers,
       time: times,
-      windspeed_10m: windspeeds,
     } = data.hourly;
     return {
       temperature: temperatures[i],
       cloudcover: cloudcovers[i],
       time: times[i],
-      windspeed: windspeeds[i],
     };
   });
   const {
     temperature_2m: temperature,
     cloudcover: cloudcover,
     time: time,
-    windspeed_10m: windspeed,
   } = data.hourly_units;
-  state.units = { temperature, cloudcover, time, windspeed };
-  console.log(state.hoursData);
+  state.units = { temperature, cloudcover, time };
 });
 </script>
